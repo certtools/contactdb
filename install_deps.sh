f3ae65d8f1f56974381c819060af2dcdf0d0f648
wolfgang <wolfgang@ubuntu-dolly.(none)>
2014-05-20 11:45:57 +0100

    Fixed install_deps.sh script.

--- install_deps.sh
+++ install_deps.sh
@@ -3,8 +3,17 @@
 set -e
 set -x
 
+CODENAME=`lsb_release -c | awk '{print $2;}'`
+SOURCE="deb http://apt.postgresql.org/pub/repos/apt/ $CODENAME-pgdg main"
+
+sudo bash -c "echo '$SOURCE' >/etc/apt/sources.list.d/pgdg.list"
+sudo bash -c 'wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -'
+
+sudo apt-get update
 sudo apt-get install postgresql-9.3
 sudo apt-get install postgresql-server-dev-9.3
+sudo apt-get install python-virtualenv
+sudo apt-get install python-dev
 sudo sed -i "s/^\(local[ ]*all[ ]*all.*\)peer/\1trust/" /etc/postgresql/9.3/main/pg_hba.conf
 sudo service postgresql restart
 
