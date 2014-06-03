902821c1ebb1ff1cf17fe1ed5efb00c13951c80c
wolfgang <wolfgang@ubuntu-dolly.(none)>
2014-06-03 15:07:08 +0100

    By mistake uploaded my version of install_deps instead of the generic one.

--- install_deps.sh
+++ install_deps.sh
@@ -9,16 +9,16 @@ SOURCE="deb http://apt.postgresql.org/pub/repos/apt/ $CODENAME-pgdg main"
 sudo bash -c "echo '$SOURCE' >/etc/apt/sources.list.d/pgdg.list"
 sudo bash -c 'wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -'
 
-#sudo apt-get update
-#sudo apt-get install postgresql-9.3
-#sudo apt-get install postgresql-server-dev-9.3
-#sudo apt-get install python-virtualenv
-#sudo apt-get install python-dev
-#sudo sed -i "s/^\(local[ ]*all[ ]*all.*\)peer/\1trust/" /etc/postgresql/9.3/main/pg_hba.conf
-#sudo service postgresql restart
+sudo apt-get update
+sudo apt-get install postgresql-9.3
+sudo apt-get install postgresql-server-dev-9.3
+sudo apt-get install python-virtualenv
+sudo apt-get install python-dev
+sudo sed -i "s/^\(local[ ]*all[ ]*all.*\)peer/\1trust/" /etc/postgresql/9.3/main/pg_hba.conf
+sudo service postgresql restart
 
-#sudo su - postgres -c 'createuser -s contactdb'
-sudo su - postgres -c 'dropdb contactdb'
+sudo su - postgres -c 'createuser -s contactdb'
+#sudo su - postgres -c 'dropdb contactdb'
 sudo su - postgres -c 'createdb contactdb'
 
 virtualenv virtenv
