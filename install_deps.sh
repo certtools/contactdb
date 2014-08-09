00a27410ea1930ed81c24a71e622f21a55306914
Raphaël Vinot <raphael@vinot.info>
2014-08-09 17:32:28 +0200

    Make it easyer to support Wheezy.

--- install_deps.sh
+++ install_deps.sh
@@ -3,12 +3,18 @@
 set -e
 set -x
 
+# Debian Wheezy has 9.1
+#POSTGRES_VERSION="9.1"
+
+# Ubuntu 14.04 has 9.3
+POSTGRES_VERSION="9.3"
+
 sudo apt-get update
-sudo apt-get install postgresql-9.3
-sudo apt-get install postgresql-server-dev-9.3
+sudo apt-get install postgresql-${POSTGRES_VERSION}
+sudo apt-get install postgresql-server-dev-${POSTGRES_VERSION}
 sudo apt-get install python-virtualenv
 sudo apt-get install python-dev
-sudo sed -i "s/^\(local[ ]*all[ ]*all.*\)peer/\1trust/" /etc/postgresql/9.3/main/pg_hba.conf
+sudo sed -i "s/^\(local[ ]*all[ ]*all.*\)peer/\1trust/" /etc/postgresql/${POSTGRES_VERSION}/main/pg_hba.conf
 sudo service postgresql restart
 
 sudo su - postgres -c 'createuser -s contactdb'
