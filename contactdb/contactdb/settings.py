38feeef8c1676324d084a9471283ac4d959e6be2
aaronkaplan <aaron@lo-res.org>
2016-11-07 15:43:55 +0100

    make initial admin interface

--- contactdb/contactdb/settings.py
+++ contactdb/contactdb/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'contacts',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -76,8 +77,9 @@ WSGI_APPLICATION = 'contactdb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'contactdb',
+        'USER': 'aaron'
     }
 }
 
